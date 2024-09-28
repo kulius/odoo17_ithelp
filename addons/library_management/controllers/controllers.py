3549b9a56b120d8c934a5ec3b26a7c7788543c2d
at2166 <Mydream23>
2024-09-28 10:47:30 +0800

    Day15：個案一:學校圖書管理(11)-網站查詢圖書

--- addons/library_management/controllers/controllers.py
+++ addons/library_management/controllers/controllers.py
@@ -4,10 +4,17 @@ from odoo.http import request
 class LibraryWebsite(http.Controller):
 
     @http.route(['/library/books'], type='http', auth='public', website=True)
-    def list_books(self, **kwargs):
-        books = request.env['library.book'].search([])
+    def list_books(self, search='', **kwargs):
+        domain = []
+        if search:
+            domain += ['|', '|',
+                       ('name', 'ilike', search),
+                       ('author', 'ilike', search),
+                       ('isbn', 'ilike', search)]
+        books = request.env['library.book'].search(domain)
         return request.render('library_management.library_book_list_template', {
             'books': books,
+            'search': search,
         })
 
     @http.route(['/library/book/<model("library.book"):book>'], type='http', auth='public', website=True)
